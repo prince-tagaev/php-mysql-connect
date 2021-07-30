64d2fc60ae0de9342f9878a1fd887ceffe35b89e
prince-tagaev <asaba@internet.ru>
2021-07-31 01:03:04 +0300

    Update dbconfig.php

--- admin/dbconfig.php
+++ admin/dbconfig.php
@@ -1,8 +1,8 @@
 <?php
 // mysqli_connect("192.168.43.61","prince.tagaev","Prince211101","library");
-$host = "192.168.43.61";
-$username = "prince.tagaev";
-$password = "Prince211101";
+$host = "localhost";
+$username = "zuhridin";
+$password = "Prince211101$";
 $data_base = "library";
 $data_connect = mysqli_connect($host, $username, $password, $data_base);
 ?>
